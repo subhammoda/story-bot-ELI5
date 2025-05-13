d63754f20750b725a727b8f1bf52da3bbdd0f867
Subham Moda <subham.moda4@gmail.com>
2025-05-13 18:38:20 -0500

    crew code to run agents

---
+++ storybotcrew.py
@@ -0,0 +1,28 @@
+from crewai import Crew, LLM
+from storybot import StoryBot
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class StoryBotCrew():
+
+    def __init__(self, topic: str) -> None:
+        self.topic = topic
+
+    def run_crew(self) -> str:
+        sb = StoryBot()
+
+        crew = Crew(
+            agents = [sb.research_agent, sb.simplifier_agent, sb.storywriter_agent, sb.educator_agent],
+            tasks = [sb.research_task, sb.simplify_task, sb.story_task, sb.review_task],
+            verbose = False
+        )
+
+        result = crew.kickoff(inputs={"topic": self.topic})
+
+        print(result)
+
+if __name__ == "__main__":
+    topic = input("Enter a topic you would like a story about: ")
+    sbc = StoryBotCrew(topic)
+    sbc.run_crew()
